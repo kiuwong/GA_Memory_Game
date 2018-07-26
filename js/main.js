ad8d8868702da6b39115713e6b544afb16c70753
kwong <kiumingwong@Kius-MBP.fios-router.home>
2018-07-26 19:51:36 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
-// defining cards
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// defining cards array
+var cards = ['queen', 'queen','king','king','queen', 'queen','king','king'];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[3];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
 // action for user to flip cards
 console.log("User Flipped " + cardOne);
 console.log("User Flipped " + cardTwo);
+
+// match logic
+var len = cardsInPlay.length;
+
+if (len === 2) {
+    console.log("good"); 
+    if(cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match");
+    } else {
+        alert("Sorry, try again!");
+    }
+}
