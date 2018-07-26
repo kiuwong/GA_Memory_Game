10b4afb00abfd5221049cf69fc233246988405f4
kwong <kiumingwong@Kius-MBP.fios-router.home>
2018-07-26 01:12:00 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// defining cards
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+// action for user to flip cards
+console.log("User Flipped " + cardOne);
+console.log("User Flipped " + cardTwo);
