c1b74005fd40e03806ec1d5301d27a690c46828c
kwong <kiumingwong@Kius-MBP.fios-router.home>
2018-07-27 19:41:16 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 // defining cards array
-var cards = ['queen', 'queen','king','king','queen', 'queen','king','king'];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 
 // defining user card selected
 var cardsInPlay = [];
@@ -7,15 +29,17 @@ var cardsInPlay = [];
 // match logic
 var checkForMatch = function() {
     if(cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match");
+        // alert("You found a match");
     } else {
-        alert("Sorry, try again!");
+        // alert("Sorry, try again!");
     }
 };
 // logic to flip card
 var flipCard = function (cardId) {
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log(cards[cardId].suit);
+    console.log(cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
     checkForMatch();
 };
 
